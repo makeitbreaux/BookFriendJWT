25e121221f344bbff93eefe19ceb2892749dcb73
makeitbreaux <makeitbreaux@gmail.com>
2022-04-08 22:13:20 -0500

    trying fix

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = require("express").Router()
+const router = express.Router()
 const pool = require("../db")
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
