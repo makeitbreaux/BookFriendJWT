e710887e1ba8e958da930647ba80d3caa1433c49
makeitbreaux <makeitbreaux@gmail.com>
2022-04-14 17:30:13 -0500

    Put all directories back in correct spots

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const cors = require("cors");
 //MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //ROUTES
 //REGISTER AND LOGIN ROUTES
@@ -18,9 +22,6 @@ app.use("/dashboard", require("./routes/dashboard"));
 //USER PROFILE ROUTES
 app.use("/users", require("./routes/user"))
 
-//This will create a middleware.
-//When you navigate to the root page, it would use the built react-app
-app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
