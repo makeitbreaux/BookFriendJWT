489525d51ea119d3a98bda327e230724c6edba3d
makeitbreaux <makeitbreaux@gmail.com>
2022-04-14 18:02:56 -0500

    removed build folder from gitignore

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 //ROUTES
