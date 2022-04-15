4e290c6ab1cb1d41dca7e38cca484ee1ae0bd7a3
makeitbreaux <makeitbreaux@gmail.com>
2022-04-15 13:57:48 -0500

    edited routes in jwtAuth and added new build folder

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user: "postgres",
-    password: "Greyson2010",
-    host: "localhost",
+    user: "pdjfavydvtvggf",
+    password: "670239d9dd1acd2fcb7fb3ff6c19b51c541500a31281bfea25b16fed844fa868",
+    host: "ec2-3-230-122-20.compute-1.amazonaws.com",
     port: 5432,
-    database: "bookfriend"
+    database: "dcv1g06b0r0kgl"
 });
 
 module.exports = pool;
--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -9,10 +9,10 @@ const authorization = require("../middleware/authorization");
 // * REGISTER USER
 router.post("/register", validInfo, async (req, res) => {
         //1. DESTRUCTURE REQ.BODY => (NAME, EMAIL, PASSWORD)
-    const {user_first_name, user_last_name, email, password} = req.body;
+        const {user_first_name, user_last_name, user_email, user_password} = req.body;
     try {
         // 2. CHECK IF USER EXISTS => (IF USER EXISTS, THROW ERROR)
-        const user = await pool.query(`SELECT * FROM users WHERE user_email = $1`, [email]);
+        const user = await pool.query(`SELECT * FROM users WHERE user_email = $1`, [user_email]);
 
         if(user.rows.length > 0) {
             return res.status(401).json("User Already Exists");
@@ -21,14 +21,14 @@ router.post("/register", validInfo, async (req, res) => {
         //3. BCRYPT USER PASSWORD
         const saltRound = 10;
         const salt = await bcrypt.genSalt(saltRound);
-        const bcryptPassword = await bcrypt.hash(password, salt);
+        const bcryptPassword = await bcrypt.hash(user_password, salt);
 
         //4. ENTER THE NEW USER INTO DB
-        let newUser = await pool.query(`INSERT INTO users (user_first_name, user_last_name, user_email, user_password) VALUES ($1, $2, $3, $4)`, [user_first_name, user_last_name, email, bcryptPassword]);
+        let newUser = await pool.query(`INSERT INTO users (user_first_name, user_last_name, user_email, user_password) VALUES ($1, $2, $3, $4)`, [user_first_name, user_last_name, user_email, bcryptPassword]);
         
         //5. GENERATING OUR JWT TOKEN
         const token = jwtGenerator(newUser.rows[0].user_id);
-        return res.send({token});
+        return res.json({token});
         
     } catch (error) {
         console.error(error.message);
@@ -40,18 +40,18 @@ router.post("/register", validInfo, async (req, res) => {
 // * LOGIN ROUTE
 router.post("/login", validInfo, async (req, res) => {
     //1. DESTRUCTURE REQ.BODY => (EMAIL, PASSWORD)
-    const {email, password} = req.body;
+    const {user_email, user_password} = req.body;
     
     try {
         //2. CHECK USER DOESN'T EXIST => (IF NOT THROW ERROR)
-        const user = await pool.query(`SELECT * FROM users WHERE user_email = $1`, [email]);
+        const user = await pool.query(`SELECT * FROM users WHERE user_email = $1`, [user_email]);
 
         if (user.rows.length === 0) {
             return res.status(401).json("Password and/or Email incorrect");
         }
 
         //3. CHECK IF INCOMING PASSWORD === DB PASSWORD
-        const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+        const validPassword = await bcrypt.compare(user_password, user.rows[0].user_password);
 
         if (!validPassword) {
             return res.status(401).json("Password and/or Email is incorrect")
@@ -59,7 +59,7 @@ router.post("/login", validInfo, async (req, res) => {
 
         //4. GIVE USER JWT TOKEN
         const token = jwtGenerator(user.rows[0].user_id);
-        return res.send({token});
+        return res.json({token});
 
     } catch (error) {
         console.error(error.message);
