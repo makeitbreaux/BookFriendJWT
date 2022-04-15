6ae9fc5df0a36d3c7f5197024ca2f34fda826042
makeitbreaux <makeitbreaux@gmail.com>
2022-04-15 16:14:46 -0500

    new build

--- server/db.js
+++ server/db.js
@@ -9,7 +9,20 @@
 //     database: "dcv1g06b0r0kgl"
 // });
 const { Client } = require("pg");
-let DB_URI = "postgres://pdjfavydvtvggf:670239d9dd1acd2fcb7fb3ff6c19b51c541500a31281bfea25b16fed844fa868@ec2-3-230-122-20.compute-1.amazonaws.com:5432/dcv1g06b0r0kgl";
-const client = new Client(DB_URI);
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+let client = new Client({
+    username: process.env.DB_USERNAME,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOSTNAME,
+    port: 5432,
+    ssl: true,
+    dialect: 'postgres',
+    dialectOptions: {
+      "ssl": {"require":true }
+    }
+})
+
+client.connect();
 
 module.exports = client;
